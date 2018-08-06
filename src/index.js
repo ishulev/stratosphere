30ae055fe89e8eda34c10b84c6b3b1132d970904
Ivan Shulev <ishulev@melontech.com>
2018-08-06 18:31:11 +0300

    add nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './styles.scss';
+
+const ACTIVE_CLASS = 'active';
+
+function clearActives(items) {
+    items.forEach(function(item) {
+        item.parentNode.classList.remove(ACTIVE_CLASS);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    const navItems = document.querySelectorAll('nav a');
+    navItems.forEach(function(ele) {
+        ele.addEventListener('click', function (e) {
+            e.preventDefault();
+            clearActives(navItems);
+            this.parentElement.classList.toggle(ACTIVE_CLASS);
+        });
+    });
+    window.addEventListener('scroll', function(e) {
+        console.log(e);
+    })
+});
